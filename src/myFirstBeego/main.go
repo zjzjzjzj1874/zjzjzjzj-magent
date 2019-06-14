0313c5be0199cbe5b7202804342fa892d337104c
zjzjzjzj1874@gmail.com <zjzjzjzj1874@gmail.com>
2019-06-14 23:03:18 +0800

    fix error in hotfix

--- src/myFirstBeego/main.go
+++ src/myFirstBeego/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/astaxie/beego/logs"
 	_ "myFirstBeego/routers"
 	"github.com/astaxie/beego"
@@ -8,6 +9,7 @@ import (
 
 func main() {
 	logs.Info("\r\n 1111 from main.go, init Methods test in github")
+	fmt.Println("I'm is hotfix changing")
 	beego.Run()
 }
 
