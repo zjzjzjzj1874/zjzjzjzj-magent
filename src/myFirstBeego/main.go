3e071aafa91e50faa8e85114910f50b2bdb3ce33
zjzjzjzj1874@gmail.com <zjzjzjzj1874@gmail.com>
2019-06-15 14:04:37 +0800

    master中增加打印信息

--- src/myFirstBeego/main.go
+++ src/myFirstBeego/main.go
@@ -9,9 +9,11 @@ import (
 
 func init()  {
 	fmt.Println("我是master分支中的init方法")
+	fmt.Println("master中增加打印信息")
 }
 func hotfix()  {
 	fmt.Println("我是hotfix分支中的hotfix函数")
+	fmt.Println("master中增加打印信息")
 }
 func main() {
 	logs.Info("\r\n 1111 from main.go, init Methods test in github")
